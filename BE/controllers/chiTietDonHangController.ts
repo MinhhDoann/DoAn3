f902820a12a38c9dc0306539318e2e3951e1b3cb
MinhDoann <doan16042005@gmail.com>
2026-05-20 16:29:12 +0700

    up

--- BE/controllers/chiTietDonHangController.ts
+++ BE/controllers/chiTietDonHangController.ts
@@ -28,22 +28,22 @@ export const deleteChiTietDonHang = async (req: Request, res: Response) => {
     const id = Number(req.params.id);
     try {
         const pool = await connectDB();
-        
+
         const transaction = new sql.Transaction(pool);
         await transaction.begin();
-        
+
         try {
             const itemResult = await transaction.request()
                 .input('id', sql.Int, id)
                 .query('SELECT ma_san_pham FROM ChiTietDonHang WHERE ma_chi_tiet = @id');
-            
+
             if (itemResult.recordset.length > 0) {
                 const item = itemResult.recordset[0];
                 await transaction.request()
                     .input('ma_san_pham', sql.Int, item.ma_san_pham)
                     .query('UPDATE SanPham SET so_luong_ton = so_luong_ton + 1 WHERE ma_san_pham = @ma_san_pham');
             }
-            
+
             const result = await transaction.request()
                 .input('id', sql.Int, id)
                 .query('DELETE FROM ChiTietDonHang WHERE ma_chi_tiet = @id');
